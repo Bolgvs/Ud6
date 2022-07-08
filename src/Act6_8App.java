41cdd22fed34f396792501c1c100b5e70845d2b6
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-08 14:53:46 +0200

    Fin ex 8

--- src/Act6_8App.java
+++ src/Act6_8App.java
@@ -1,8 +1,40 @@
+import javax.swing.JOptionPane;
+
 public class Act6_8App {
 	
 	public static void main (String [] args) {
 		
+		int array[] = new int[10];
 		
+		rellenarValores(array);
+		
+		mostrarValores(array);
+		
+	}
+	
+	public static void rellenarValores (int array[]) {
+		
+		// Declaramos la variable "bucle"
+		
+		int bucle;
+		
+		for (bucle = 0; bucle < array.length; bucle++)
+			
+			array[bucle] = Integer.parseInt(JOptionPane.showInputDialog("Di un numero"));
+		
+	}
+	
+	public static void mostrarValores (int array[]) {
+		
+		// Declaramos la variable "bucle"
+		
+		int bucle;
+				
+		for (bucle = 0; bucle < array.length; bucle++) {
+		
+		JOptionPane.showMessageDialog(null, "El valor en la posición " + bucle + " es: " + array[bucle]);
+		
+		}
 		
 	}
 
